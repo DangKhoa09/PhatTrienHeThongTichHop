03908877c0403a177269b5ee23ce53495388f00a
Đăng Khoa <72064370+DangKhoa09@users.noreply.github.com>
2020-11-03 09:27:22 +0700

    Add files via upload

---
+++ IFlight.java
@@ -0,0 +1,14 @@
+package lab5;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import Flight;
+
+public interface IFlight extends Remote{
+
+	public Flight getFlight(String x) throws RemoteException;
+	public String Display(int x) throws RemoteException;
+	public String[] DataId() throws RemoteException;
+	public String Order(String x,int y,String user,String password) throws RemoteException;
+	public String Pay(String x,int y,String user,String password) throws RemoteException;
+}
